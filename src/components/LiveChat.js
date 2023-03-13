fbfcec1f896e2a187b40cd15967788095224143a
Poonam <poonam.chauhan229@gmail.com>
2023-03-13 17:17:00 +0530

    2:15 hr done

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,11 +1,22 @@
 import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import {addMessage} from '../utilis/chatSlice'
+import { generateRandomName, makeRandomMessage } from "../utilis/helper";
 import ChatMessage from "./ChatMessage"
 
+
 const LiveChat = () => {
+  const dispatch=useDispatch();
+
+  const chatMessages=useSelector(store=>store.chat.messages)
     useEffect(() => {
      const i=setInterval(()=>{
         // API Polling
-        console.log("API Polling")
+        console.log("API Polling");
+        dispatch(addMessage({
+          name:generateRandomName(),
+          message:makeRandomMessage(20),
+        }))
      },2000)
     
       return () => clearInterval(i)
@@ -13,11 +24,10 @@ const LiveChat = () => {
     
   return (
     <div className='ml-2 w-full h-[500px]
-    p-2 border border-black bg-slate-100 rounded-lg'>
-        <ChatMessage name="Poonam" message="This is React js"/>
-        <ChatMessage name="Poonam" message="This is React js"/>
-        <ChatMessage name="Poonam" message="This is React js"/>
-        <ChatMessage name="Poonam" message="This is React js"/>
+    p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll'>
+        {chatMessages.map((element)=>(
+          <ChatMessage name={element.name} message={element.message}/>
+        ))}
     </div>
   )
 }

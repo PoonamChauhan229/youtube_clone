8ae87ca2ca51a2bb0491a3829794fb2c9e4e35df
Poonam <poonam.chauhan229@gmail.com>
2023-03-15 21:29:28 +0530

    part 5 27min

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 const VideoCard = ({info}) => {
+  useEffect(()=>{
+    getVideoDetails()
+  })
+
+  async function  getVideoDetails(){
+
+  }
    // console.log(info)
     const {snippet,statistics}=info;
-    const {channelTitle,title,thumbnails}=snippet
+    const {channelId,channelTitle,title,publishedAt,thumbnails}=snippet
     return (info===undefined)?null:  (
     <div className='m-2 p-2 shadow-lg w-[283px] h-96 hover:bg-gray-300'>
         <img className='rounded-lg' src={thumbnails.medium.url} alt="thumbnail" />

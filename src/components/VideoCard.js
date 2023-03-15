696c8b04b59b2fd6f72e1dbe823cd5fb0e73ffeb
Poonam <poonam.chauhan229@gmail.com>
2023-03-15 23:51:19 +0530

    homepage videolist  done

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,24 +1,58 @@
-import React, { useEffect } from 'react'
+import moment from 'moment';
+import numeral from 'numeral';
+import React, { useEffect, useState } from 'react'
+import { API_Key } from '../utilis/constants';
 
 
 const VideoCard = ({info}) => {
+  
+  const {snippet,id}=info;
+  const {channelId,channelTitle,title,publishedAt,thumbnails}=snippet
+  const[views,setViews]=useState(null)
+  const [duration,setDuration]=useState(null)
+  const [channelIcon,setChannelIcon]=useState(null)
   useEffect(()=>{
     getVideoDetails()
-  })
+  },[id])
 
-  async function  getVideoDetails(){
+  useEffect(()=>{
+    getChannelIcon()
+  },[channelId])
 
+  async function getVideoDetails(){
+    let data=await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails%2Cstatistics&id=${id}&key=${API_Key}`)
+    let json=await data.json();
+    // console.log(json.items)    
+    setDuration(json.items[0].contentDetails.duration)
+    setViews(json.items[0].statistics.viewCount)
+  }
+
+  async function getChannelIcon(){
+    let data=await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${API_Key}`)
+    let json=await data.json();
+    console.log(json.items)
+    setChannelIcon(json.items[0].snippet.thumbnails.default.url)
   }
    // console.log(info)
-    const {snippet,statistics}=info;
-    const {channelId,channelTitle,title,publishedAt,thumbnails}=snippet
+  
+    // duration -install moment library 
+
+  const seconds=moment.duration(duration).asSeconds()
+  const newDuration=moment.utc(seconds*1000).format("mm:ss")
+
+  
     return (info===undefined)?null:  (
-    <div className='m-2 p-2 shadow-lg w-[283px] h-96 hover:bg-gray-300'>
+    <div className='m-2 p-2 shadow-lg w-[284px] h-89 hover:bg-gray-300'>
         <img className='rounded-lg' src={thumbnails.medium.url} alt="thumbnail" />
+        <span>{newDuration}</span>
         <ul>
             <li className='font-bold whitespace-nowrap	overflow-hidden text-ellipsis'>{title}</li>
-            <li className='font-semibold'>{channelTitle}</li>
-            <li className='font-medium py-2'><i class="bi bi-eye-fill "></i>{" "}{(statistics.viewCount/1000000).toFixed(1)}m Views</li>
+            <li className='font-medium py-2'><i className="bi bi-eye-fill "></i>{" "}{numeral(views).format("0.a")} Views</li>
+            <li className='font-semibold'>{moment(publishedAt).fromNow()}</li>
+            <div className='flex'>
+            <img className='rounded-full w-10 h-10' src={channelIcon} alt="thumbnail" />
+            <li className='font-semibold py-3 ml-2'>{channelTitle}</li>
+            </div>
         </ul>
     </div>
   )
--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -1,4 +1,4 @@
-const API_Key = "AIzaSyA9LK-lPhJSJuK9WdnZqBlaC_5oxQyxeMo";
+export const API_Key = "AIzaSyA9LK-lPhJSJuK9WdnZqBlaC_5oxQyxeMo";
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_Key}`;
 
 export const YOUTUBE_SEARCH_API = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
@@ -9,3 +9,5 @@ export const YOUTUBE_SEARCH_SHOWVIDEO_API=`https://youtube.googleapis.com/youtub
 // https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyA9LK-lPhJSJuK9WdnZqBlaC_5oxQyxeMo&q=%22poonam%22
 
 // https://youtube.googleapis.com/youtube/v3/search?&maxResults=10&key=AIzaSyA9LK-lPhJSJuK9WdnZqBlaC_5oxQyxeMo
+
+export const GENERIC_VIDEOS=`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails%2Cstatistics&id=id&key=${API_Key}`

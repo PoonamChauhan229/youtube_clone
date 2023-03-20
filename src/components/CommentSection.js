ccd6245232597f6dc74a95ebbba1532d523c02be
Poonam <poonam.chauhan229@gmail.com>
2023-03-21 00:03:16 +0530

    watch page css done

---
+++ src/components/CommentSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import moment from 'moment'
+const CommentSection = () => {
+  return (
+    <div className='border-b-[2px] border-grey-500 p-2 flex text-sm'>
+         <img
+            src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
+            alt=''
+            className='rounded-full mr-3 w-[50px] h-[50px]'
+         />
+         <div className=''>
+            <p className='mb-1'>
+               Sumit Dey • {moment('2020-05-05').fromNow()}
+            </p>
+            <p className='mb-0'>Nice Video DUDE!!!</p>
+         </div>
+      </div>
+  )
+}
+
+export default CommentSection
--- src/components/VideoComments.js
+++ src/components/VideoComments.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import CommentSection from './CommentSection'
 
 const VideoComments = () => {
   return (
     <div className='comments'>
          <p>1234 Comments</p>
-         <div className='comments__form d-flex w-100 my-2'>
+         <div className='flex w-full my-2'>
             <img
                src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
                alt=''
-               className='rounded-circle mr-3'
+               className='rounded-full mr-3 w-[50px] h-[50px] object-contain'
             />
-            <form onSubmit={handleComment} className='d-flex flex-grow-1'>
+            <form className='flex grow'>
                <input
                   type='text'
-                  className='flex-grow-1'
+                  className='grow input'
                   placeholder='Write a comment...'
                />
-               <button className='border-0 p-2'>Comment</button>
+               <button className='border-0 p-2 bg-slate-600 text-white hover:bg-gray-900'>Comment</button>
             </form>
          </div>
-         {/* <div className='comments__list'>
+         <div className='comments__list'>
             {[...Array(15)].map(() => (
-               <Comment />
+               <CommentSection/>
             ))}
-         </div> */}
+         </div>
       </div>
   )
 }

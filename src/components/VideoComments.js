1b1d8a6d37cf06e866e9d0fc44ca1930b3ebcdcf
Poonam <poonam.chauhan229@gmail.com>
2023-03-20 18:27:57 +0530

    watch page tailwind css

---
+++ src/components/VideoComments.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const VideoComments = () => {
+  return (
+    <div className='comments'>
+         <p>1234 Comments</p>
+         <div className='comments__form d-flex w-100 my-2'>
+            <img
+               src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
+               alt=''
+               className='rounded-circle mr-3'
+            />
+            <form onSubmit={handleComment} className='d-flex flex-grow-1'>
+               <input
+                  type='text'
+                  className='flex-grow-1'
+                  placeholder='Write a comment...'
+               />
+               <button className='border-0 p-2'>Comment</button>
+            </form>
+         </div>
+         {/* <div className='comments__list'>
+            {[...Array(15)].map(() => (
+               <Comment />
+            ))}
+         </div> */}
+      </div>
+  )
+}
+
+export default VideoComments
